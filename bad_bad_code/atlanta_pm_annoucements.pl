17bf012e4650ce185ec7e50c745b0b8219488b50
scott <scott@sboss.net>
2012-03-20 17:22:15 -0400

    brought back the original very bad code version.

---
+++ bad_bad_code/atlanta_pm_annoucements.pl
@@ -0,0 +1,33 @@
+#!perl
+#
+$|=1;
+#
+#
+my $message1 = 'welcome to the atlanta.pm Feb meeting.';
+my $message2 = 'Stephen is in charge... :)';
+my $message3 = 'please return your badges to security.';
+my $message4 = 'restrooms are to the R, R, half was down on the R.';
+my $message5 = 'dont take food from breakroom. sodas from soda fountain is ok';
+my $messageSB = 'scott is looking for Storage Engineer, Backup Admins(2), Linux SysAdmins(4), Linux Engineers(8+).';
+#
+#
+#
+while(1)
+	{
+	printf sprintf "%s\n", $message1;
+	sleep 1;
+	printf sprintf "%s\n", $message2;
+	sleep 1;
+	printf sprintf "%s\n", $message3;
+	sleep 1;
+	printf sprintf "%s\n", $message4;
+	sleep 1;
+	printf sprintf "%s\n", $message5;
+	sleep 1;
+	print "\n";
+	printf sprintf "%s\n", $messageSB;
+	sleep 1;
+	
+	print "\n\n\n\n";
+	sleep 1;
+	}
---
+++ bad_bad_code/bad_bad_code.md
@@ -0,0 +1,10 @@
+# README -  bad bad code
+
+this version is the original "bad bad code" version. it is kept around for people like Bruce (Util) to use an an example of very bad code.
+
+## syntax
+atlanta\_pm\_annoucements.pl 
+
+
+## editors
+	sboss
